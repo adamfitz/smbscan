48901ea7534010357d1f6785aa33f18208736641
Adam <adam@networknoob.net>
2023-09-16 00:51:38 +0000

    initial commit

---
+++ smbscan.py
@@ -0,0 +1,21 @@
+"""
+Objective is to take a network range, scan for open smb ports and enumerate them for username and passwords
+"""
+
+
+from sys import argv
+import ipaddress
+import socket
+
+
+
+def main():
+    """
+    scan a subnet, find targets with open smb ports, attempt to enumerate unprotected shares
+    """
+
+    
+
+    
+
+
