dbf745b326278ec174c1489f9c369408fd4c6332
Adam <adam@networknoob.net>
2023-09-17 11:42:10 +0000

    working on scan script

--- smbscan.py
+++ smbscan.py
@@ -13,6 +13,15 @@ def main(network: str):
     """
     scan a subnet, find targets with open smb ports, attempt to enumerate unprotected shares
     """
+    try:
+        # get the provided ip block and convert to ipaddress object
+        target = ipaddress.ip_network(network, strict=False)
+        for ip in target.hosts():
+            print(ip)
+            # check here if all the hosts in the provided network range are listening for smb connections
+    except ValueError as invalid_ip_block:
+        print(f"Error:\n{invalid_ip_block}")
+
 
     
 
@@ -33,4 +42,4 @@ if __name__ == "__main__":
         network = argv[1]
         main(network)
     except IndexError as missing_argument:
-        print("A network argument is required, example:\n$ smbscanner.py 10.0.0.0/24")
+        print("A network block argument in CIDR notation is required, example:\n$ smbscanner.py 10.0.0.0/24")
