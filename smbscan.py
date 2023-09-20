c463e781daa64ca7a54ebcdcc6eb39e4fe08133b
Adam <adam@networknoob.net>
2023-09-20 10:55:41 +0000

    added basic scanning

--- smbscan.py
+++ smbscan.py
@@ -8,25 +8,53 @@ import ipaddress
 import socket
 
 
-
 def main(network: str):
     """
     scan a subnet, find targets with open smb ports, attempt to enumerate unprotected shares
     """
+
+    smb_ports = [139, 445]
+    open_targets_p139 = []
+    open_targets_p445 = []
+
     try:
         # get the provided ip block and convert to ipaddress object
-        target = ipaddress.ip_network(network, strict=False)
-        for ip in target.hosts():
-            print(ip)
-            # check here if all the hosts in the provided network range are listening for smb connections
+        address_block = (ipaddress.ip_network(str(network), strict=False).hosts())
+
+        # create a generator containing each IP as string
+        hosts = map(str, address_block)
+        
+        # iterate the target network or host
+        for ip in hosts:
+            # iterate each port in the smb_ports list
+            for port in smb_ports:
+                try:
+                    # setup the connection
+                    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+                    # low timeout value so the script is quicker
+                    connection.settimeout(1)
+                    
+                    # attempt connection
+                    connection.connect((ip, int(port)))
+
+                    # port is open add target to the list
+                    if port == 139:
+                        open_targets_p139.append(ip)
+                    else:
+                        open_targets_p445.append(ip)
+                    
+                except socket.error as socket_error:
+                    continue
+                finally:
+                    # close connection after each connection attempt
+                    connection.close()
+
+        print(f"Targets listening on port 139:\n{open_targets_p139}\n")
+        print(f"Targets listening on port 445:\n{open_targets_p445}\n")
+
     except ValueError as invalid_ip_block:
-        print(f"Error:\n{invalid_ip_block}")
-
-
-    
-
-    
-
+        print(f"\nError: Invalid address block\n{invalid_ip_block}")
 
 
 if __name__ == "__main__":
